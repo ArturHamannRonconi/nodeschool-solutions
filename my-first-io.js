7c51af492341fa2dbb1ec0f470b291f73091a83c
Artur Hamann Ronconi <arturhronconi@gmail.com>
2021-04-08 16:27:05 -0300

    my first async io

--- my-first-io.js
+++ my-first-io.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 
 const filePath = process.argv[2]
 
-const contentFile = fs.readFileSync(filePath)
-const string = contentFile.toString()
+const fileContent = fs.readFileSync(filePath)
+const string = fileContent.toString()
 
 console.log(string.split('\n').length - 1)
