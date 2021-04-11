ad650a2ca83ed8fe2b03432a64a5b1ed2863b504
Artur Hamann Ronconi <arturhronconi@gmail.com>
2021-04-11 09:33:54 -0300

    adjusting

--- http-file-server.js
+++ http-file-server.js
@@ -6,9 +6,6 @@ const file = process.argv[3]
 
 const readStream = fs.createReadStream(file)
 
-const server = http.createServer((req, res) => {
-  const writable = readStream.pipe(res)
-  writable.on('close', data => console.log(data))
-})
+const server = http.createServer((req, res) => readStream.pipe(res))
 
 server.listen(port, () => console.log('Server is Runing...'))
